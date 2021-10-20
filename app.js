9095cbd57f927a5c90ec35fef4e435124676519f
Ozan2000 <akhmadfauzan1337@gmail.com>
2021-10-20 18:05:09 +0700

    Change from encryption to hashing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 ///////////// REQUIRE SECTION //////////////////////////
 
 //////////// APP USE SECTION ///////////////////////////
@@ -26,7 +27,8 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -54,7 +56,7 @@ app.get("/register", (req,res)=>{
 app.post("/register", (req,res)=>{
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -69,7 +71,7 @@ app.post("/register", (req,res)=>{
 
 app.post("/login", (req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, foundUser)=>{
         if(!err){
