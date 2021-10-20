9560f2f7264edeb46705057e1bc48c9380ae0840
Ozan2000 <akhmadfauzan1337@gmail.com>
2021-10-21 00:33:34 +0700

    changing from hash md5 to bcrypt hash along with salt

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 ///////////// REQUIRE SECTION //////////////////////////
 
 //////////// APP USE SECTION ///////////////////////////
@@ -54,33 +56,37 @@ app.get("/register", (req,res)=>{
 /////////// POST METHOD ///////////////////////////////
 
 app.post("/register", (req,res)=>{
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save((err)=>{
-        if(!err){
-            console.log("Success to add new user to database");
-            res.render("secrets");
-        }else{
-            console.log(err);
-        }
-    });
+    bcrypt.hash(req.body.password, saltRound, (err,hash)=>{
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save((err)=>{
+            if(!err){
+                console.log("Success to add new user to database");
+                res.render("secrets");
+            }else{
+                console.log(err);
+            }
+        });
+    })    
 });
 
 app.post("/login", (req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser)=>{
         if(!err){
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }else{
-                    console.log("Wrong Password");
-                }
+               bcrypt.compare(password, foundUser.password, (err, result)=>{
+                   if(result === true){
+                       res.render("secrets");
+                   }else{
+                       console.log("wrong password");
+                   }
+               });
             }else{
                 console.log("Wrong Username");
             }
