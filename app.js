acb3f017dbc30314032979e544318dad06c3e402
Ozan2000 <akhmadfauzan1337@gmail.com>
2021-10-22 23:28:32 +0700

    add OAuth google 2.0

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 ///////////// REQUIRE SECTION //////////////////////////
 
 //////////// APP USE SECTION ///////////////////////////
@@ -29,6 +31,8 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+
 //////////// APP USE SECTION ///////////////////////////
 
 //////////// MONGOOSE(MONGODB) ////////////////////////
@@ -37,10 +41,12 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
@@ -48,8 +54,35 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done)=> {
+    User.findById(id, (err, user)=> {
+      done(err, user);
+    });
+  });
+
+///////////// GOOGLE OAUTH 2.0 /////////////////////////
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+///////////// GOOGLE OAUTH 2.0 /////////////////////////
 
 //////////// MONGOOSE(MONGODB) ////////////////////////
 
@@ -59,6 +92,18 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", (req,res)=>{
     res.render("home");
 });
+
+app.get("/auth/google", 
+    passport.authenticate("google", {scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {failureRedirect: "/login"}),
+    (req,res)=>{
+        //Successfull authentication, redirect to secret route
+        res.redirect("/secrets");
+    }
+)
 
 app.get("/login", (req,res)=>{
     res.render("login");
